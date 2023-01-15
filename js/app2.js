6df7f3c1ea086d45ba323c1db6d61bd601ee8ad4
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-01-15 15:04:59 +0700

    dededed

--- js/app2.js
+++ js/app2.js
@@ -30,8 +30,9 @@ $('.autoplay').slick({
     {
       breakpoint: 480,
       settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToShow: 1.05,
+        slidesToScroll: 1,
+        centerMode: true
       }
     }
     // You can unslick at a given breakpoint now by adding:
