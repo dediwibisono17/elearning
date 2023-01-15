e548c5e4cdc4e48837382302c5afafaa11f8bb5c
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-01-15 14:41:32 +0700

    deded

--- js/app2.js
+++ js/app2.js
@@ -1,4 +1,11 @@
-$(document).ready(function(){
-    new WOW().init()
-  })
-  
+$(document).ready(function () {
+  new WOW().init()
+})
+
+
+$('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
