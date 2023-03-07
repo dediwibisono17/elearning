c23ef4af8a6a9d3e4bec659e0ff41f7b30370f48
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-03-07 13:01:37 +0700

    update

--- js/app2.js
+++ js/app2.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     new WOW().init()
 })
 
@@ -7,7 +7,7 @@ $('.wraps').slick({
     infinite: true,
     speed: 300,
     arrows: true,
-    fade:true,
+    fade: true,
     // autoplay: true,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -21,45 +21,52 @@ $('.autoplay').slick({
     autoplay: true,
     autoplaySpeed: 2000,
     responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                centerMode: true
-            }
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
         }
+    },
+    {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 2
+        }
+    },
+    {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: true
+        }
+    }
         // You can unslick at a given breakpoint now by adding:
         // settings: "unslick"
         // instead of a settings object
     ]
 });
 
-$(".toapp").click(function() {
+$(".toapp").click(function () {
     $('html, body').animate({
         scrollTop: $(".download").offset().top
     }, 500);
 });
 
-$('ul.menu').find('a').click(function(){
+$('ul.menu').find('a').click(function () {
     var $href = $(this).attr('href');
     // alert($href)
     var $anchor = $($href).offset();
-    $('body,html').animate({ scrollTop: $anchor.top -40 },200);
+    $('body,html').animate({ scrollTop: $anchor.top - 40 }, 200);
     // return false;
+});
+
+$(document).ready(function () {
+    $('ul.menu li a').click(function () {
+        $('li a').removeClass("active");
+        $(this).addClass("active");
+    });
 });
